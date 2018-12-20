ac289bc44d1a05e531b610e20e46b06ea4344e67
Marko Kosunen <marko.kosunen@aalto.fi>
2018-12-19 19:05:26 -0800

    Publish git-versioned submodule f2_interpolator

---
+++ init_submodules.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#Init submodules in this dir, if any
+DIR="$( cd "$( dirname $0 )" && pwd )"
+git submodule update --init
+
+#Publish local the ones you need
+for module in clkdiv_n_2_4_8 halfband_interpolator cic3_interpolator; do
+    cd $DIR/${MODULE}
+    sbt publishLocal
+done
+
+#Selectively, init submodules of a larger projects
+#cd $DIR/rocket-chip
+#git submodule update --init chisel3
+#git submodule update --init firrtl
+#git submodule update --init hardfloat
+
+#Recursively update submodeles
+#cd $DIR/eagle_serdes
+#git submodule update --init --recursive serdes_top
+#sbt publishLocal
+#Assemble executables
+###sbt publishing
+#cd $DIR/rocket-chip/firrtl
+#sbt publishLocal
+#sbt assembly
+
+exit 0
